582a4be72505cc5daf8213df9b43e2ab5c367bc4
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-04-02 20:10:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,58 +11,44 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *head,*curr1,*curr2,*new2,*new1=new ListNode();
-        curr1=list1;
-        curr2=list2;
-       head=nullptr;
+        if(list1==nullptr)return list2;
+        if(list2==nullptr)return list1;
+        int val;
+        ListNode *head,*new1,*new2;
+        head=nullptr;
 
-       if(list1==nullptr)return list2;
-       if(list2==nullptr)return list1;
-
-        while(curr1!=nullptr && curr2!=nullptr){
-            int val;
-            if(curr1->val <= curr2->val){
-                val=curr1->val;
-                curr1=curr1->next;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                val=list1->val;
+                list1=list1->next;
             }
             else{
-                val=curr2->val;
-                curr2=curr2->next;
+                val=list2->val;
+                list2=list2->next;
             }
-
             if(head==nullptr){
                 head=new ListNode(val);
                 new1=head;
             }
             else{
-            new2=new ListNode(val);
-            new1->next=new2;
-            new1=new2;
+                new2 = new ListNode(val);
+                new1->next=new2;
+                new1=new2;
             }
         }
-        
-        while(curr1!=nullptr){
-            new2=new ListNode(curr1->val);
+
+        while(list1){
+            new2=new ListNode(list1->val);
             new1->next=new2;
             new1=new2;
-            curr1=curr1->next;
-         
+            list1=list1->next;
         }
-
-         while(curr2!=nullptr){
-            new2=new ListNode(curr2->val);
+        while(list2){
+            new2=new ListNode(list2->val);
             new1->next=new2;
             new1=new2;
-            // if(curr2==nullptr){
-            //     new1->next=nullptr;
-            //     break;
-            // }
-            curr2=curr2->next;
-            
+            list2=list2->next;
         }
-
         return head;
-
-        
     }
 };

18031e24188e3c2a51acd0c2583b6e0a77567674
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-04-07 22:59:30 +0530

    Time: 0 ms (100%), Space: 8 MB (97.77%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+
+        for( char c : word){
+            if(isupper(c))count++;
+        }
+
+        if(count==word.size() || count==0)return true;
+        if(count==1 && isupper(word[0]))return true;
+        return false;
+    }
+};

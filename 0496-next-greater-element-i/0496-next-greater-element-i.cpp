de3ecfd473c4f9010c80f3fa3da3ce81b0c3b7aa
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-04-07 11:44:32 +0530

    Time: 0 ms (100%), Space: 12.8 MB (35.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
 
-    int find(int i,int j, vector<int>& nums2){
+    // int find(int i,int j, vector<int>& nums2){
         
-        for(int k=i;k<nums2.size();k++){
-            if(nums2[k]>j) return nums2[k];
-        }
-        return -1;
-    }
+    //     for(int k=i;k<nums2.size();k++){
+    //         if(nums2[k]>j) return nums2[k];
+    //     }
+    //     return -1;
+    // }
+
+    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int> ans1;
+    //     unordered_map<int,int> ans;
+        
+    //     for(int i=0;i<nums2.size();i++){
+    //         int j=find(i,nums2[i],nums2);
+    //         ans[nums2[i]]=j;
+    //     }
+
+    //     for(int i : nums1){
+    //         if(ans.count(i)){
+    //             ans1.push_back(ans[i]);
+    //         }
+    //         else{
+    //             ans1.push_back(-1);
+    //         }
+    //     }
+    //     return ans1;
+
+    // }
 
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans1;
-        unordered_map<int,int> ans;
-        
+
+        stack<int> st;
+        unordered_map<int,int> mp;
+        vector<int> ans;
         for(int i=0;i<nums2.size();i++){
-            int j=find(i,nums2[i],nums2);
-            ans[nums2[i]]=j;
-        }
-
-        for(int i : nums1){
-            if(ans.count(i)){
-                ans1.push_back(ans[i]);
+            while(!st.empty() && st.top() < nums2[i]){
+                mp[st.top()]=nums2[i];
+                st.pop();
             }
-            else{
-                ans1.push_back(-1);
-            }
+            st.push(nums2[i]);
         }
-        return ans1;
-
+        while(!st.empty()){
+            mp[st.top()]=-1;
+            st.pop();
+        }
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
     }
 };

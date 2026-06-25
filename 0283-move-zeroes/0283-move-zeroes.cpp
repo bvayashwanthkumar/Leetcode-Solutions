51c2145dd8564f47d4c53ce4ac8612db2fff6806
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-06-25 09:24:46 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int pass=0,index=0;
-        if(nums.size()==1)return;
-        while(index<=nums.size()-1){
-            if(nums.size()==pass)nums[index++]=0;
-            else if(nums[pass]==0)pass++;
-            else nums[index++]=nums[pass++];
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
        
     }

e478d89ca54add9722573811899e777c6b002a8b
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-07-13 09:21:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char> ans;
-       for(char a:s){
-            if(a=='(' || a=='{' || a=='[')ans.push(a);
-            else if(ans.empty()) return false;
-            else if ((a==')' && ans.top()=='(')||(a=='}' && ans.top()=='{') || (a==']' && ans.top()=='['))ans.pop();
-            else return false;
-       }
-       if(ans.size()==0)return true;
-       return false;
+        stack<char>st;
+        for(char c:s){
+            if(c=='('||c=='['||c=='{'){
+                st.push(c);
+            }
+            else if(st.empty())return false;
+            else if((st.top()=='(' && c==')') || (st.top()=='{' && c=='}') || (st.top()=='[' && c==']')){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+            
+        }
+        if(st.size()==0)return true;
+        else return false;
     }
 };

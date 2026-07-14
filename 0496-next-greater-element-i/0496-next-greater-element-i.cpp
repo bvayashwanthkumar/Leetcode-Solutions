9aeb2534e24e9efe0cae4a63d976eb085e404c14
Yashwanth Kumar BVA <150168153+bvayashwanthkumar@users.noreply.github.com>
2026-07-14 13:59:06 +0530

    Time: 2 ms (47.13%), Space: 13 MB (15.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,53 +1,18 @@
 class Solution {
 public:
-
-    // int find(int i,int j, vector<int>& nums2){
-        
-    //     for(int k=i;k<nums2.size();k++){
-    //         if(nums2[k]>j) return nums2[k];
-    //     }
-    //     return -1;
-    // }
-
-    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    //     vector<int> ans1;
-    //     unordered_map<int,int> ans;
-        
-    //     for(int i=0;i<nums2.size();i++){
-    //         int j=find(i,nums2[i],nums2);
-    //         ans[nums2[i]]=j;
-    //     }
-
-    //     for(int i : nums1){
-    //         if(ans.count(i)){
-    //             ans1.push_back(ans[i]);
-    //         }
-    //         else{
-    //             ans1.push_back(-1);
-    //         }
-    //     }
-    //     return ans1;
-
-    // }
-
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-
-        stack<int> st;
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        for(int i=0;i<nums2.size();i++){
-            while(!st.empty() && st.top() < nums2[i]){
-                mp[st.top()]=nums2[i];
-                st.pop();
-            }
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        int n=nums2.size();
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i])st.pop();
+            if(st.empty())mp[nums2[i]]=-1;
+            else mp[nums2[i]]=st.top();
             st.push(nums2[i]);
         }
-        while(!st.empty()){
-            mp[st.top()]=-1;
-            st.pop();
-        }
-        for(int i=0;i<nums1.size();i++){
-            ans.push_back(mp[nums1[i]]);
+        for(int i : nums1){
+            ans.push_back(mp[i]);
         }
         return ans;
     }
